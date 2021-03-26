c8e47ac897420f19b785bfb4e457c5e4b9894374
grd23GrantDavis <77860562+grd23GrantDavis@users.noreply.github.com>
2021-03-26 18:01:23 -0400

    Add files via upload

---
+++ waysideController.py
@@ -0,0 +1,22 @@
+import time
+from PyQt5 import QtCore, QtGui, QtWidgets
+import tkinter as tk
+from tkinter import filedialog
+
+class waysideController(object):
+    def waysideControllers(self):
+        return 1
+    def setPLCinputs(self, blockOccupancies, blockAuthorities, faultStatuses, underground):
+        return 1
+
+    def runPLC(self, file_path):
+        return 1
+
+    def getTrafficLightStatus(self):
+        return 1
+
+    def getBlockAuthorities(self):
+        return 1
+
+    def getSwitchPositions(self):
+        return 0
