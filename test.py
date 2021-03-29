d6f9b1d5445106a716f7a5c22794784b46cb4527
jsp69 <77817645+jsp69@users.noreply.github.com>
2021-03-28 23:59:43 -0400

    Update test.py

--- test.py
+++ test.py
@@ -51,7 +51,7 @@ if __name__=='__main__':
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui=Ui_MainWindow()
+    ui=trackModel()
     ui.setupUi(MainWindow)
     
     unittest.main()
