cb76038f0c12501c1d764a0b7636db310b4c49d2
Jason Matuszak <57767833+JasonM3687@users.noreply.github.com>
2021-03-31 18:16:17 -0400

    Add files via upload

--- HWTrainController_V1.ino
+++ HWTrainController_V1.ino
@@ -1,6 +1,9 @@
+
 //Written by Jason Matuszak
 //University of Pittsburgh ECE 1140
 
+#include <timer.h>
+#include <timerManager.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <Keypad.h> 
@@ -20,30 +23,52 @@ byte rowPins[ROWS] = {23, 22, 25, 26};
 byte colPins[COLS] = {29, 30, 33, 34}; 
 
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
-String keypad_input;
+String keypad_input = "";
 String trainSpeed = "000";
-String kp;
-String ki;
+String kp = "";
+String ki = "";
 String temperature = "075";
-String power;
+String calculatedPower = "";
 //-------------------------------------------------------
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); //Set LCD I2C Address for LCD
 
 //Input/output status to be sent to train controller 
-String headLights;
-String internalLights;
-String doors;
-String serviceBrake;
-String emergencyBrake;
-String automaticMode;
-String trainModelSpeed;
+String headLights = "";
+String internalLights = "";
+String doors = "";
+String serviceBrake = "";
+String emergencyBrake = "";
+String automaticMode = "";
+String trainModelSpeed = "";
+String suggestedSpeed = "";
+String authority = "";
+String beacon = "";
+String limit = "";
+String faultStatus = "";
+String automaticSpeed = "";
+unsigned long startMillis;  
+unsigned long currentMillis;
+unsigned long changeMillis;
+double error = 0.0;
+double error2 = 0.0;
+double errorSum = 0.0;
+double errorSum2 = 0.0;
+double maxPower = 150000;
+double currentVelocity = 10;
+double power = 0.0;
+int intSpeed;
+int intKp;
+int intKi;
 
 
 void setup() {
-  Serial.begin(2000000);
+  
+  Serial.begin(9600);
   keypad_input.reserve(3); //Reserves 4 inputs max per keypad input
 
   lcd.begin(20, 4); //Initializes cols/rows of LCD
+
+  startMillis = millis(); 
 
 //-------------------- Digital Output/Input Setup --------------------
   //Sets digital pin 49 as output (Headlights)
@@ -54,8 +79,8 @@ void setup() {
   pinMode(44, OUTPUT);
   //Sets digital pin 6 as output (Emergency Break)
   pinMode(6, OUTPUT);
-  //Sets digital pin 3 as output (Open/Close Doors)
-  pinMode(3, OUTPUT);
+  //Sets digital pin 4 as output (Open/Close Doors)
+  pinMode(4, OUTPUT);
   //Sets digital pin 40 as output (Intercom)
   pinMode(40, OUTPUT);
   //Sets digital pin 11 as output (Automatic Mode)
@@ -83,10 +108,21 @@ void setup() {
 }
 
 void loop() {
-  int read37 = digitalRead(37); 
+  
+   // if (Serial.available() > 0)
+  //{
+ //   String firstRead = Serial.readStringUntil(';');
+ //   limit = firstRead;
+ //  suggestedSpeed = Serial.readStringUntil(';');
+ //   authority = Serial.readStringUntil(';');
+  //  beacon = Serial.readStringUntil(';');
+  //  faultStatus = Serial.readStringUntil(';');
+ // }
  
+  int read37 = digitalRead(37); 
  // If engine is on
- if (read37 == HIGH) {
+ if (read37 == HIGH) { 
+  
     
   setKeypadLCD();
   
@@ -97,10 +133,11 @@ void loop() {
   setDoors();
   setIntercom();
   setAutomaticMode();
-  testHeadLights("1");
-  testInternalLights("1");
-  testTrainSpeedLimit("150");
-  testDoors("1");
+  calculatePower();
+//  testHeadLights("1");
+//  testInternalLights("1");
+//  testTrainSpeedLimit(limit);
+//  testDoors("1");
 
 }
 else
@@ -114,13 +151,13 @@ String setHeadlights() {
   if (read53 == HIGH) {
        digitalWrite(49, HIGH);
        headLights = "1";
-       Serial.print(headLights);
+       Serial.print("Headlights:" + headLights + "\n");
        return headLights;
   }
   else if (read53 == LOW) {
        digitalWrite(49,LOW);
        headLights = "0";
-       Serial.print(headLights);
+       Serial.print("Headlights:" + headLights + "\n");
        return headLights;
   }
 
@@ -132,13 +169,13 @@ String setInternalLights() {
   if (read52 == HIGH) {
       digitalWrite(48, HIGH);
       internalLights = "1";
-      Serial.print(internalLights);
+      Serial.print("Internal Lights:" + internalLights + "\n");
       return internalLights;
   }
   else if (read52 == LOW) {
       digitalWrite(48, LOW);
       internalLights = "0";
-      Serial.print(internalLights);
+    Serial.print("Internal Lights:" + internalLights + "\n");
       return internalLights;
   }
 }
@@ -149,14 +186,14 @@ void setServiceBrake() {
   if (read45 == HIGH) {
       digitalWrite(44, HIGH);
       serviceBrake = "1";
-      Serial.print(serviceBrake);
-      delay(2000);
+      Serial.print("Service Brake:" + serviceBrake + "\n");
+      delay(3000);
       trainSpeed = "000";
   }
   else if (read45 == LOW) {
       digitalWrite(44, LOW);
       serviceBrake = "0";
-      Serial.print(serviceBrake);
+       Serial.print("Service Brake:" + serviceBrake + "\n");
   }
 }
 
@@ -166,7 +203,7 @@ void setEmergencyBrake() {
  if (read5 == HIGH ) {
       //Sets Emergency Break high
       emergencyBrake = "1'";
-      Serial.print(emergencyBrake);
+      Serial.print("Emergency Brake:" + emergencyBrake + "\n");
       digitalWrite(6, HIGH);
       //Waits 1/4 Sec
       delay(250);
@@ -181,7 +218,7 @@ void setEmergencyBrake() {
  else if (read5 == LOW) {
       digitalWrite(6, LOW);
       emergencyBrake = "0";
-      Serial.print(emergencyBrake);
+      Serial.print("Emergency Brake:" + emergencyBrake + "\n");
  }
 }
 
@@ -189,15 +226,15 @@ String setDoors() {
   int read2 = digitalRead(2);
   //Opens doors if switch is pressed
  if (read2 == HIGH) {
-      digitalWrite(3, HIGH);
+      digitalWrite(4, HIGH);
       doors = "1";
-      Serial.print(doors);
+      Serial.print("Doors:" + doors + "\n");
       return doors;
  }
  else if (read2 == LOW) {
-      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
       doors = "0";
-      Serial.print(doors);
+      Serial.print("Doors:" + doors + "\n");
       return doors;
  }
 }
@@ -207,7 +244,7 @@ void setIntercom(){
   //Turns on intercom for driver to speak if button is pressed
  if (read41 == HIGH) {
       digitalWrite(40, HIGH);
-      Serial.print("Transit Driver is Speaking...\n");
+      //Serial.print("Transit Driver is Speaking...\n");
  }
  else if (read41 == LOW) {
       digitalWrite(40, LOW);
@@ -219,13 +256,40 @@ void setAutomaticMode() {
   //Initiliazes Automatic Mode
  if (read13 == HIGH) {
       digitalWrite(11, HIGH);
-      trainSpeed = trainModelSpeed;
+      lcd.setCursor(6, 0);
+      lcd.print(suggestedSpeed);
      // Serial.print("Automatic Mode Initialized\n");
  }
  else if (read13 == LOW) {
       digitalWrite(11, LOW);
  }
 
+}
+
+void calculatePower() {
+  currentMillis = millis();
+  intSpeed = trainSpeed.toInt();
+  intKp = kp.toInt();
+  intKi = ki.toInt();
+
+  changeMillis = currentMillis - startMillis;
+
+  error = intSpeed - currentVelocity;
+
+  if (power > maxPower) {
+      errorSum = errorSum2 + (changeMillis/2) * (error + error2);
+  }
+  else
+    errorSum = errorSum2;
+
+  power = (intKp * error) + (intKi * errorSum);
+  calculatedPower = String(power);
+  Serial.print("Power:" + calculatedPower + "\n");
+
+  startMillis = currentMillis;
+  error2 = error;
+  errorSum2 = errorSum;
+  
 }
 
 void setKeypadLCD() {
@@ -261,17 +325,17 @@ void setKeypadLCD() {
   lcd.setCursor(9,0);
   lcd.print("MPH");
   lcd.setCursor(0, 1);
-  lcd.print("Power:00000W");
+  lcd.print("Power:" + calculatedPower + "W");
   lcd.setCursor(0, 2);
-  lcd.print("BCN: Pittsburgh,PA");
+  lcd.print("BCN: " + beacon);
   lcd.setCursor(0, 3);
-  lcd.print("FS: N/A");
+  lcd.print("FS: " + faultStatus);
   lcd.setCursor(6, 0);
   lcd.print(trainSpeed);
-  lcd.setCursor(13,1);
-  lcd.print(kp);
-  lcd.setCursor(17,1);
-  lcd.print(ki);
+ // lcd.setCursor(15,1);
+ // lcd.print(kp);
+ // lcd.setCursor(18,1);
+ // lcd.print(ki);
   lcd.setCursor(13,0);
   lcd.print("Tmp:");
   lcd.setCursor(17,0);
@@ -280,45 +344,45 @@ void setKeypadLCD() {
 
 //-------------------------------------------------------Tests----------------------------------------------------------------
 //Testing if headlights are on
-void testHeadLights(String output) {
-    Serial.print("Testing Headlights["); 
-    Serial.print(setHeadlights());
-    Serial.print("]: ");
-    Serial.println((setHeadlights() == output) ? "Pass" : "Fail");
-}
+//void testHeadLights(String output) {
+//    Serial.print("Testing Headlights["); 
+//    Serial.print(setHeadlights());
+//    Serial.print("]: ");
+//    Serial.println((setHeadlights() == output) ? "Pass" : "Fail");
+//}
 
 //Testing if internal lights are on
-void testInternalLights(String output) {
-    Serial.print("Testing Internal Lights["); 
-    Serial.print(setInternalLights());
-    Serial.print("]: ");
-    Serial.println((setInternalLights() == output) ? "Pass" : "Fail");
-}
+//void testInternalLights(String output) {
+//    Serial.print("Testing Internal Lights["); 
+//    Serial.print(setInternalLights());
+//    Serial.print("]: ");
+//    Serial.println((setInternalLights() == output) ? "Pass" : "Fail");
+//}
 
 //Testing that speed cannot exceed speed limit
-void testTrainSpeedLimit(String limit) {
-    int speedint;
-    speedint = trainSpeed.toInt();
-    Serial.print("Testing Speed["); 
-    Serial.print(trainSpeed);
-    Serial.print("]: ");
-    Serial.println((trainSpeed <= limit) ? "Pass" : "Fail");
-    if (trainSpeed > limit) {
-      Serial.print("TRAIN IS OVER SPEED LIMIT, EMERGENCY BRAKE, SLOWING DOWN");
-        trainSpeed = "000";
-    }
-}
+//void testTrainSpeedLimit(String limit) {
+ //   int speedint;
+ //   speedint = trainSpeed.toInt();
+ //   Serial.print("Testing Speed["); 
+ //   Serial.print(trainSpeed);
+ //   Serial.print("]: ");
+ //   Serial.println((trainSpeed <= limit) ? "Pass" : "Fail");
+ //   if (trainSpeed > limit) {
+ //     Serial.print("TRAIN IS OVER SPEED LIMIT, EMERGENCY BRAKE, SLOWING DOWN");
+ //       trainSpeed = "000";
+ //   }
+//}
 
 //Testing that doors cannot be opened while train is in motion
-void testDoors(String output) {
+//void testDoors(String output) {
     
-    Serial.print("Testing Doors["); 
-    Serial.print(trainSpeed);
-    Serial.print("]: ");
-    Serial.println((trainSpeed == "000" & doors == output ) ? "Pass" : "Fail");
-    if (trainSpeed != "000" & doors == "1") {
-      Serial.print("TRAIN IS IN MOTION WITH DOORS OPEN, CLOSING DOORS");
-        doors = "0";
-        digitalWrite(3, LOW);
-    }
-}
+//    Serial.print("Testing Doors["); 
+//    Serial.print(trainSpeed);
+//    Serial.print("]: ");
+//    Serial.println((trainSpeed == "000" & doors == output ) ? "Pass" : "Fail");
+//    if (trainSpeed != "000" & doors == "1") {
+//      Serial.print("TRAIN IS IN MOTION WITH DOORS OPEN, CLOSING DOORS");
+//        doors = "0";
+//        digitalWrite(3, LOW);
+//    }
+//}
